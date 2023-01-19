4e6bfc6905ecc9ec77693c1407a566ae6967d367
lorenajulio <83107691+lorenajulio@users.noreply.github.com>
2023-01-19 17:20:23 -0500

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+//Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+// Tu nombre (string)
+const nombre = "Lorena Julio";
+
+
+//Tu edad (number)
+var edad = 22;
+
+
+//¿Eres desarrollador? (boolean)
+var desarrollador = true;
+
+//Tu fecha de nacimiento (Date)
+
+//Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)//
+var libro={
+    titulo :'Orgullo y Prejuicio',
+    autor :'Jane Austen' ,
+    fecha :'1813',
+    url:'https://www.panamericana.com.co/orgullo-y-prejuicio-3-/p'
+}
+
